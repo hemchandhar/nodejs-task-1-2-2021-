adfedc28bebc5a59585d2a9f130e3fb94f75466c
hemchandhar <hemchandhar@gmail.com>
2021-02-10 20:07:48 +0530

    console log removed and project completed

--- routes/controllers/crud.js
+++ routes/controllers/crud.js
@@ -4,7 +4,6 @@ const alert = require("alert");
 
 //CREATE Operation
 module.exports.postAttendance = (req, res) => {
-  console.log(req.body.customRadio);
   const sql = "INSERT INTO user (id,name, date,attendance) VALUES (?,?,?,?)";
   db.query(
     sql,
@@ -55,17 +54,14 @@ module.exports.postUsers = (req, res) => {
   const { name, date, attendance } = req.body;
   const id = req.params.id;
   const sql = "UPDATE user SET name=?,date=?,attendance=? WHERE id=?";
-  console.log(id);
   db.query(sql, [name, date, attendance, id], function (err) {
     if (err) {
-      console.log(err);
       res.render("toast", {
         message: "Error in Updating the Response! Please try again",
         buttonName: "Back to Admin",
         link: "/admin",
       });
     } else {
-      console.log("Values Successfully Updated");
       res.render("toast", {
         message: "The Response is Updated",
         buttonName: "Back to Admin",
@@ -78,17 +74,14 @@ module.exports.postUsers = (req, res) => {
 module.exports.deleteUsers = (req, res) => {
   const id = req.params.id;
   const sql = "DELETE FROM user WHERE id=?";
-  console.log(id);
   db.query(sql, [id], function (err) {
     if (err) {
-      console.log(err);
       res.render("toast", {
         message: "Error in Deleting the Response! Please try again",
         buttonName: "Back to Admin",
         link: "/admin",
       });
     } else {
-      console.log("Values Deleted Successfully");
       res.render("toast", {
         message: "The Response is Deleted",
         buttonName: "Back to Admin",
