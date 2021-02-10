9937b07f1c8940e621ab40023790caad216d9966
hemchandhar <hemchandhar@gmail.com>
2021-02-10 10:58:05 +0530

    edit functionality added

--- routes/controllers/crud.js
+++ routes/controllers/crud.js
@@ -1,17 +1,8 @@
 const db = require("../../models/db");
 const { v4: uuidv4 } = require("uuid");
 
-//CREATE operation
+//CREATE Operation
 module.exports.postAttendance = (req, res) => {
-  console.log(req.body.name);
-  console.log(req.body.date);
-  console.log(req.body.customRadio1);
-  console.log(req.body.customRadio2);
-  // if (req.body.customRadio1 == "on") {
-  //   const attendance = "Present";
-  // } else {
-  //   const attendance = "Absent";
-  // }
   const sql = "INSERT INTO user (id,name, date,attendance) VALUES (?,?,?,?)";
   db.query(
     sql,
@@ -23,22 +14,61 @@ module.exports.postAttendance = (req, res) => {
     ],
     function (err, data) {
       if (err) {
-        console.log(err);
         res.render("toast", {
           message: "Error in Recording your Response! Please try again",
+          buttonName: "Back",
+          link: "/",
         });
       } else {
-        console.log("Successfully Created");
-        res.render("toast", { message: "Your Response is Recorded" });
+        res.render("toast", {
+          message: "Your Response is Recorded",
+          buttonName: "Back",
+          link: "/",
+        });
       }
     }
   );
 };
 
+//READ Operation
 module.exports.getAdmin = (req, res) => {
-  var sql = "SELECT * FROM user";
+  const sql = "SELECT * FROM user";
   db.query(sql, function (err, data) {
     if (err) throw err;
     res.render("admin", { userData: data });
+  });
+};
+
+module.exports.getUsers = (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM user WHERE id=?";
+  db.query(sql, [id], function (err, data) {
+    if (err) throw err;
+    res.render("editUsers", { userData: data });
+  });
+};
+
+//UPDATE Operation
+module.exports.postUsers = (req, res) => {
+  const { name, date, attendance } = req.body;
+  const id = req.params.id;
+  const sql = "UPDATE user SET name=?,date=?,attendance=? WHERE id=?";
+  console.log(id);
+  db.query(sql, [name, date, attendance, id], function (err, data) {
+    if (err) {
+      console.log(err);
+      res.render("toast", {
+        message: "Error in Updating your Response! Please try again",
+        buttonName: "Back to Admin",
+        link: "/admin",
+      });
+    } else {
+      console.log("Values Successfully Updated");
+      res.render("toast", {
+        message: "Your Response is Recorded",
+        buttonName: "Back to Admin",
+        link: "/admin",
+      });
+    }
   });
 };
--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,8 @@ router.post("/", crudController.postAttendance);
 
 router.get("/admin", crudController.getAdmin);
 
+router.get("/users/edit/:id", crudController.getUsers);
+
+router.post("/user/update/:id", crudController.postUsers);
+
 module.exports = router;
